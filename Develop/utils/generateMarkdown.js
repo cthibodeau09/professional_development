e2f6f6fbfaec359f70c899cd48fb732e569653a8
Little Bit <cthibodeau09@gmail.com>
2023-04-14 20:51:38 -0400

    Added coding for Licenses

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,43 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `
+    [![License](https://img.shields.io/badge/License-${license}-blue.svg)]
+    `;
+  } else {
+    return ' ';
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function that returns the license link
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+    `;
+  } else {
+    return ' ';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that returns the license section of README
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `
+    
+    The application is covered under the following license:
+    
+    ${renderLicenseLink(license)}
+    `;
+  } else {
+    return ' ';
+  }
+}
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
 
 ## Table of Contents
 - [Description](#description)
@@ -47,3 +72,4 @@ ${data.testing}
 }
 
 module.exports = generateMarkdown;
+
